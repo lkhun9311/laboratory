89dc01d63d22d650695cf536c0b35977eecdccaa
kwanghunlee <lkhun9311@naver.com>
2024-11-18 21:39:02 +0900

    resolve: [BaekJoon] 2750_SortingNumbers

--- CodingInterview/BaekJoon/Python/2750_SortingNumbers.py
+++ CodingInterview/BaekJoon/Python/2750_SortingNumbers.py
@@ -24,10 +24,29 @@ print(f"Execution time for solution1: {(end_time - start_time)*1000:.6f} ms")
 
 # solution1 최적화 
 def solution2(input):
-    return sorted(input)
+  return sorted(input)
 
 start_time = time.time()
 print("\n[solution2]")
 print(solution2(input1))
 end_time = time.time()
 print(f"Execution time for solution2: {(end_time - start_time)*1000:.6f} ms")
+
+def solution3(input):
+  result = input
+
+  for idx in range(len(input)):
+    min_index = idx
+    for j in range(idx+1, len(input)):
+      if result[min_index] > result[j]:
+        min_index = j
+    # 핵심. Swap 활용     
+    result[idx], result[min_index] = result[min_index], result[idx]
+
+  return result
+
+start_time = time.time()
+print("\n[solution3]")
+print(solution3(input1))
+end_time = time.time()
+print(f"Execution time for solution3: {(end_time - start_time)*1000:.6f} ms")
