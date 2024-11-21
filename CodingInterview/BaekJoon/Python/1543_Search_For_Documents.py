37c56304297c88de68499625d8eaf00deca52f52
kwanghunlee <lkhun9311@naver.com>
2024-11-21 22:36:48 +0900

    resolve: [BaekJoon] 1543_Search_For_Documents

--- CodingInterview/BaekJoon/Python/1543_Search_For_Documents.py
+++ CodingInterview/BaekJoon/Python/1543_Search_For_Documents.py
@@ -32,3 +32,31 @@ print(solution1(input3))
 print(solution1(input4))
 end_time = time.time()
 print(f"Execution time for solution1: {(end_time - start_time)*1000:.6f} ms")
+
+# solution1 코드 최적화
+# 핵심. str.find() 내장 함수 활용
+def solution2(input):
+  answer = 0
+  source, word = input
+  idx = 0
+  space = len(word)
+
+  while True:
+    idx = source.find(word, idx)
+
+    if idx == -1:
+      break
+
+    answer += 1
+    idx += space
+
+  return answer
+
+start_time = time.time()
+print("[solution2]")
+print(solution2(input1))
+print(solution2(input2))
+print(solution2(input3))
+print(solution2(input4))
+end_time = time.time()
+print(f"Execution time for solution2: {(end_time - start_time)*1000:.6f} ms")
