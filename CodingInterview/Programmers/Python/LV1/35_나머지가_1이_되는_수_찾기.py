d52f1ce5eed14095f961f6cb67aab817a857df65
lkhun9311 <lkhun9311@naver.com>
2026-07-17 22:40:26 +0900

    resolve: [Programmers] 나머지가 1이 되는 수 찾기
    풀이 헤더(횟수/날짜) 갱신 누락분 반영

--- CodingInterview/Programmers/Python/LV1/35_나머지가_1이_되는_수_찾기.py
+++ CodingInterview/Programmers/Python/LV1/35_나머지가_1이_되는_수_찾기.py
@@ -1,7 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/87389?language=python3
 
-# Number of times solving questions: 0
-# 2026-00-00
+# Number of times solving questions: 1
+# 2026-07-04
 
 import sys, pathlib
 sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))  # test_runner 경로
