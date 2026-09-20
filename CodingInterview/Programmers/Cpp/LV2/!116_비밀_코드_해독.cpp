d15424353413862d128b95b121bfd1f43f587477
lkhun9311 <lkhun9311@naver.com>
2026-09-20 22:25:07 +0900

    chore: 전수 조사로 확인한 누락 문제 9개 스켈레톤 추가

---
+++ CodingInterview/Programmers/Cpp/LV2/!116_비밀_코드_해독.cpp
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/388352?language=cpp
+// 출처: 2025 신규 추가분 (2026-09-20 전수 조사로 로컬 누락 확인)
+// ※ 레벨은 외부 블로그 분류 기준이라 공식 등급을 재검증하지 못했다.
+
+// Number of times solving questions: 0
+// 2026-00-00
+
+#include "../test_runner.h"
+// 아래는 프로그래머스 제출 코드와 동일하게 유지한다.
+// 필요한 헤더는 직접 추가할 것 (<algorithm> sort/min_element, <numeric> accumulate,
+// <sstream> stringstream, <unordered_map>, <unordered_set>, <set>, <stdexcept> runtime_error)
+#include <string>
+#include <vector>
+using namespace std;
+
+// TODO: 프로그래머스 C++ 답안지 스켈레톤의 시그니처를 그대로 옮겨 적을 것.
+
+int main() {
+    Tester t("solution");
+    // TODO: 문제의 입출력 예로 t.check(...) 를 채울 것
+    return t.report();
+}
